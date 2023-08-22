2ce1d3006360fa88bb6a2dbd0e4e3d43db37f899
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-08-21 20:14:04 -0400

    commits

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
 import { useSelector, useDispatch } from 'react-redux'
+import { toggleDark, toggleLight } from './features/modeSlice';
 
 function App() {
   
   const mode = useSelector((state) => state.mode)
   const dispatch = useDispatch()
 
+  const toggleMode = () => {
+    mode.darkMode
+    ? dispatch(toggleLight())
+    : dispatch(toggleDark())
+  }
+
   return (
     <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
       <Nav />
+      <button onClick={toggleMode}></button>
       <ContentWrapper />
       <Footer />
     </div>
